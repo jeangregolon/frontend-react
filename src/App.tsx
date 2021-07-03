a0c36159be00fc10234923428c72466ce5b8b8f8
Jean Gregolon <jean.gregolon@gmail.com>
2021-07-03 16:34:34 -0300

    table layout done

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
 				<main>
 					<Route path='/' component={PostsScreen} exact />
 				</main>
-				
+
 			</BrowserRouter>
 		</div>
 	)
--- src/screens/PostsScreen.tsx
+++ src/screens/PostsScreen.tsx
@@ -4,18 +4,63 @@ import '../index.scss'
 const PostsScreen: React.FC = () => {
     return (
         <div className='posts-container'>
-            <h1>Page Title</h1>
-            <h2>Table Header</h2>
-            <h2>Post</h2>
-            <h2>Post</h2>
-            <h2>Post</h2>
-            <h2>Post</h2>
-            <h2>Post</h2>
-            <h2>Post</h2>
-            <h2>Post</h2>
-            <h2>Post</h2>
-            <h2>Post</h2>
-            <h1>Pagination</h1>
+            <h2>Últimas Postagens</h2>
+            <div className='table-container'>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Título</th>
+                            <th>Conteúdo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Título</td>
+                            <td>Conteúdo</td>
+                        </tr>
+                        <tr>
+                            <td>Exibindo 9 postagens</td>
+                            <td>1 | 2 | 3 </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
